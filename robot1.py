b8b94b1a914d78734211f8070ef01815846ef57b
Shayne <shaynetremblay@hotmail.com>
2015-05-17 15:20:50 -0400

    .

--- robot1.py
+++ robot1.py
@@ -8,10 +8,10 @@ gpio.setup(11, gpio.OUT)
 gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
 
-gpio.output(7, 1)
+gpio.output(7, 0)
 gpio.output(11, 0)
-gpio.output(13, 0)
-gpio.output(15, 1)
+gpio.output(13, 1)
+gpio.output(15, 0)
 time.sleep(0.5)
 
 gpio.cleanup()
