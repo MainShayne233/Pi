ceb2c4d931e277075847098532a9ae25d49bc425
Shayne <shaynetremblay@hotmail.com>
2015-05-17 15:09:36 -0400

    .

--- carcontrol.py
+++ carcontrol.py
@@ -60,14 +60,14 @@ def backwardright(tf):
         gpio.cleanup()
 
 def pivotleft(tf):
-	print "pivotleft"
-	gpio.output(7, 1)
-	gpio.output(11, 0)
-	gpio.output(13, 0)
-	gpio.output(15, 1)
-	time.sleep(tf)
-	gpio.cleanup()
+		gpio.output(7, 0)
+        gpio.output(11, 0)
+        gpio.output(13, 1)
+        gpio.output(15, 0)
+        time.sleep(tf)
+        gpio.cleanup()
 
+        
 def key_input(event):
 	init()
 	print 'Key: ', event.char
